7abe62cab0974be463987ad2a9a708959d58823c
Nikhiy <nikhilchowdary2006@gmail.com>
2026-03-18 22:32:16 +0530

    dont till login part

--- services/auth/src/controllers/auth.ts
+++ services/auth/src/controllers/auth.ts
@@ -1,8 +1,12 @@
+import axios from "axios";
+import getBuffer from "../utils/buffer.js";
 import { sql } from "../utils/db.js";
 import ErrorHandler from "../utils/errorHandler.js";
 import { TryCatch } from "../utils/TryCatch.js";
 import bcrypt from 'bcrypt';
-
+import dotenv from 'dotenv'
+dotenv.config()
+import jwt from "jsonwebtoken";
 export const registerUser=TryCatch(async(req,res,next)=>{
     const {name,password,phoneNumber,role,bio,email}=req.body;
     if(!name || !password || !phoneNumber ||!role || !email){
@@ -13,7 +17,7 @@ export const registerUser=TryCatch(async(req,res,next)=>{
     if(existingUsers.length>=1){
         throw new ErrorHandler(409,"User with this email already exists")
     }
-    const hashPassword=bcrypt.hash(password,10);
+    const hashPassword=await bcrypt.hash(password,10);
     let registeredUser;
     if(role==="recruiter"){
         const [user]=await sql`INSERT INTO users (name,email,password,phone_number,role) VALUES
@@ -23,8 +27,63 @@ export const registerUser=TryCatch(async(req,res,next)=>{
     }
     else if(role==="jobseeker"){
         const file=req.file
-        const [user]=await sql`INSERT INTO users (name,email,password,phone_number,role) VALUES
-        (${name},${email},${hashPassword},${phoneNumber},${role}) RETURNING user_id,name,email,phone_number,role,created_at`;
+        if(!file){
+            throw new ErrorHandler(400,"Resume File is required for job seekers")
     }
-    res.json(email)
+
+        const fileBuffer=getBuffer(file)
+        if(!fileBuffer || !fileBuffer.content){
+            throw new ErrorHandler(500,"Failed to generate file buffer")
+        }
+
+        const {data}=await axios.post(`${process.env.UPLOAD_SERVICE}/api/utils/upload`,{
+            buffer:fileBuffer.content
+        })
+        const [user]=await sql`INSERT INTO users (name,email,password,phone_number,role,bio,resume,resume_public_id) VALUES
+        (${name},${email},${hashPassword},${phoneNumber},${role},${bio},${data.url},${data.public_id}) RETURNING user_id,name,email,phone_number,role,bio,resume,created_at`;registeredUser=user
+    }
+
+    const token=jwt.sign({id:registeredUser?.user_id},process.env.JWT_SEC as string,{
+        expiresIn:"15d"
+    })
+
+    res.status(201).json({
+        message:"User registered succesfully",
+        registeredUser,
+        token,
+        });
+}) 
+
+export const loginUser=TryCatch(async(req,res,next)=>{
+    const {email,password}=req.body;
+    if(!email || !password){
+        throw new ErrorHandler(400,"Please Fill all the details")
+    }
+
+    const user=await sql`
+    SELECT u.user_id,u.name,u.email,u.password,u.phone_number,u.role,u.bio,u.resume,u.profile_pic,u.subscription,ARRAY_AGG(s.name) FILTER (WHERE s.name IS NOT NULL) as skills FROM users u LEFT JOIN user_skills us ON u.user_id=us.user_id
+    LEFT JOIN skills s ON us.skill_id=s.skill_id
+    WHERE u.email =${email} GROUP BY u.user_id;
+    `;
+    if(user.length===0){
+        throw new ErrorHandler(400,"INvalid Credentials")
+    }
+
+    const userObject=user[0];
+    const matchPassword=await bcrypt.compare(password,userObject.password)
+    if(!matchPassword){
+        throw new ErrorHandler(400,"Invalid Credentials")
+    }
+    userObject.skills=userObject.skills || [];
+    delete userObject.pass
+    const token=jwt.sign({
+        id:userObject?.user_id
+    },process.env.JWT_SEC as string,{
+        expiresIn:"15d"
+    })
+    res.json({
+        message:"user LoggedIn",
+        userObject,
+        token
+    })
 })
